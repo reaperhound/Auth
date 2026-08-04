65ad9bce4777e94dbabe8f67d6a3449ccba550cb
Ajmal Khan <ajmal@nextbraintech.com>
2026-08-04 19:15:26 +0530

    feat(cookies-read): done

--- JS/jwt-auth/src/auth/auth.controller.ts
+++ JS/jwt-auth/src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
+import type { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -12,7 +20,60 @@ export class AuthController {
   }
 
   @Post('/login')
-  async login(@Body() body: SignupDto) {
-    return this.authService.login(body);
+  async login(
+    @Body() body: SignupDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken, refreshToken, message } =
+      await this.authService.login(body);
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 15 * 60 * 1000, // 15m
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
+    });
+
+    return { message };
+  }
+
+  @Post('/refresh')
+  async refresh(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refreshToken: string = req.cookies?.refreshToken as string;
+    console.log({ refreshToken });
+    if (!refreshToken)
+      throw new UnauthorizedException('No refresh token provided');
+
+    const { accessToken, refreshToken: newRefeshToken } =
+      await this.authService.refresh({
+        refreshToken,
+      });
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 15 * 60 * 1000,
+    });
+
+    res.cookie('refreshToken', newRefeshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      path: '/auth/refresh',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return { message: 'Token refreshed' };
   }
 }
