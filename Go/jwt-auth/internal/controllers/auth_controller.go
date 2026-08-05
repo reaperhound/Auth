bc0a2de448f593d307b7e557347f408e8d033d11
Ajmal Khan <ajmal@nextbraintech.com>
2026-08-05 13:09:56 +0530

    feat(controller-go): reads body an throws err

--- Go/jwt-auth/internal/controllers/auth_controller.go
+++ Go/jwt-auth/internal/controllers/auth_controller.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"encoding/json"
+	"jwt-auth/internal/models"
 	"jwt-auth/internal/services"
 	"net/http"
+	"strings"
 )
 
 type UserController struct {
@@ -16,7 +19,23 @@ func NewUserController(service *services.AuthService) *UserController {
 }
 
 func (c *UserController) SignUp(w http.ResponseWriter, r *http.Request) {
-	response := c.authService.SignUp()
+	var user models.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
 
-	w.Write([]byte(response))
+	if strings.TrimSpace(user.Username) == "" || strings.TrimSpace(user.Password) == "" {
+		http.Error(w, "username and password are required", http.StatusBadRequest)
+		return
+	}
+
+	authErr := c.authService.SignUp(user.Username, user.Password)
+	if authErr != nil {
+		http.Error(w, authErr.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Signed up"))
 }
--- Go/jwt-auth/internal/services/auth_service.go
+++ Go/jwt-auth/internal/services/auth_service.go
@@ -1,6 +1,11 @@
 package services
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"jwt-auth/internal/models"
+	"os"
+)
 
 type AuthService struct{}
 
@@ -8,8 +13,40 @@ func NewAuthService() *AuthService {
 	return &AuthService{}
 }
 
-func (s *AuthService) SignUp() string {
-	const msg string = "Signed up"
-	fmt.Println(msg)
-	return msg
+func (s *AuthService) SignUp(username, password string) error {
+	user := models.User{
+		Username: username,
+		Password: password,
+	}
+
+	err := s.appendUser(user)
+	if err != nil {
+		return fmt.Errorf("failed to sign up user: %w", err)
+	}
+
+	fmt.Println("Signed up")
+	return nil
+}
+
+func (s *AuthService) appendUser(user models.User) error {
+	const fileName = "users.json"
+
+	var users []models.User
+
+	// Read
+	data, err := os.ReadFile(fileName)
+	if err == nil {
+		json.Unmarshal(data, &users)
+	}
+
+	// Add new user
+	users = append(users, user)
+
+	// Convert back to json
+	updatedData, err := json.MarshalIndent(users, "", " ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, updatedData, 0644)
 }
